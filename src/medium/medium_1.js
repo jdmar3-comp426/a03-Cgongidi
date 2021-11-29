c5fd092992b2d098eae0ef321d68bb9aa6dd4a6b
Chetan <gongidic@live.unc.edu>
2021-11-29 18:41:40 -0500

    Submission initialization test

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,10 +8,10 @@ import {variance} from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
-var sum = 0;
-for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-}
+    var sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
 }
 
 
